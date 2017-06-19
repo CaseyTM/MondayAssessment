d38376e629940afc51648542edb6d875f7404d4f
Casey McCaskill <>
2017-06-19 14:27:34 -0400

    hardmode

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const loremIpsum = require('lorem-ipsum');
 
 
 
-app.get('/lorem',function(req,res){
-res.send(loremIpsum());
+app.get('/lorem/:limit',function(req,res){
+res.send(loremIpsum({count:req.params.limit}));
+console.log(req);
 });
 app.listen(3000,function(){
 	console.log("Herro, this server is running and listening on port 3000");
